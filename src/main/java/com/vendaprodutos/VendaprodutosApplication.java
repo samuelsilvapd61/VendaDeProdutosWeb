cf7624f51b63dd88214b61284eaa98dd0e669c16
Samuel de Oliveira Silva <samuelsilvapd61@gmail.com>
2022-06-20 12:51:18 -0300

    Criação do Projeto. Adicionado POM com depedências básicas.

---
+++ src/main/java/com/vendaprodutos/VendaprodutosApplication.java
@@ -0,0 +1,13 @@
+package com.vendaprodutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendaprodutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendaprodutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vendaprodutos/VendaprodutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vendaprodutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendaprodutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
