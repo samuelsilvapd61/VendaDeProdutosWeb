3f78759b70b518cffb5c382b9b782c66aae012b2
Samuel de Oliveira Silva <samuelsilvapd61@gmail.com>
2022-06-27 16:56:51 -0300

    Criação do domain Produto. Conserto do ClienteMapper.

--- target/generated-sources/annotations/com/vendaprodutos/mapper/CategoriaMapperImpl.java
+++ target/generated-sources/annotations/com/vendaprodutos/mapper/CategoriaMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-06-27T15:08:39-0300",
+    date = "2022-06-27T16:31:55-0300",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.3 (Azul Systems, Inc.)"
 )
 @Component
